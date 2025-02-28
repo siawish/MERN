a86a5a4ae8376143f2fabed5c5a09f43891da137
siawish <01-131232-025@student.bahria.edu.pk>
2025-02-28 22:42:57 +0500

    Objects && Arrays in JS

---
+++ lecture44/index.js
@@ -0,0 +1,51 @@
+//   let obj={
+//     Name :"Love",
+//     age :25,
+//     weight: 20,
+//     height:"6ft 1in",
+//     greet :function(){
+//             console.log("Hello Jee Kese Ho sare");
+//     }
+//   };
+
+//  for (const key in obj) {
+//     console.log(key, obj[key]);
+       
+//     }
+ 
+//   console.log(obj)
+//   obj.greet();
+
+// let arr= {};
+// arr=[1,2,3,4,5,6];
+// console.log(arr);
+// arr.push(13);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.unshift(1,2);
+// console.log(arr);
+
+let arr=[10,20,30,11,21,44,51];
+
+// console.log(arr.map((number, index )=> {
+//     return number*number,index;
+// }));
+
+// console.log(arr.filter(number => {
+//    return number%2 ===0;
+// }));
+
+// console.log(
+//     arr.reduce ((acc,curr) => {
+//     return acc+curr;
+// },0));
+
+// arr.forEach((value, index)=>{
+//     console.log("Number:", value, 
+//         "Index:", index
+//     );
+// })
+
