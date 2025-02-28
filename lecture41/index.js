85b4ee5e0ede7204ec31d09661c81ec4244e9e20
siawish <01-131232-025@student.bahria.edu.pk>
2025-02-28 17:43:30 +0500

    Operators and Conditionals

---
+++ lecture41/index.js
@@ -0,0 +1,16 @@
+//  let a=10;
+//  let b=2;
+//  console.log(a+"2");
+
+//  console.log(1 ^  5 );
+
+// console.log(10<<1);
+
+//--------------conditionals 
+// let age= 13;
+// if (age>=10){
+//     console.log("can vote");
+// }
+// else
+//     console.log("cannot vote");
+
