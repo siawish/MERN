a977003b19828804c6bc2878ff023837b8a111d9
siawish <01-131232-025@student.bahria.edu.pk>
2025-02-28 21:17:27 +0500

    functions

---
+++ lecture43/index.js
@@ -0,0 +1,45 @@
+// 
+
+//basic syntax 
+// function sayMyName(){
+//     console.log("malik siawish");
+// }
+
+// // function use- function call 
+// sayMyName();
+
+// function printTable(num){
+//     for(x=1; x<11;x++){
+//         let  printTableRow=`${num} x ${x} = ${num*x}`;
+//         console.log(printTableRow);
+//     }
+// }
+// let num=10;
+// console.log(`table of ${num}`, num ,"ka table");
+// printTable(num);
+
+//return functions 
+
+// function namel(name){
+//     return name;
+// }
+
+// console.log(namel(1));
+
+// function getMyName(firstName, lastName){
+//     return firstName+lastName;
+// }
+
+// console.log("Full Name =",getMyName("malik ","siawish"));
+
+// let powerfunc=function(a,b){
+//     return a**b;
+// }
+
+// console.log(powerfunc("2","10"));
+
+let getExp = (x,y)=>{
+    return x**y;
+}
+
+console.log(getExp(2,10));
