dbff1fff37fca6e8f5dccf9c77c7206dd7de25ae
siawish <01-131232-025@student.bahria.edu.pk>
2025-02-28 11:59:59 +0500

    js-variables

---
+++ lecture40/index.js
@@ -0,0 +1,28 @@
+// var age = 25;
+
+// if (true){
+    
+//     console.log(age); 
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+
+// }
+
+// solve();
+
+// {let a=10;
+//     console.log(a);
+// }
+
+// let a=10;
+// let a=20; // not possible
+// const a=10;
+// console.log(a);
+
+
+let ali ="siawish";
+console.log(typeof(ali));
+
