7c1fcc3cd95fbc164e5b06c2ebd954e3610a73de
siawish <01-131232-025@student.bahria.edu.pk>
2025-02-28 18:32:52 +0500

    strings

---
+++ lecture42/index.js
@@ -0,0 +1,12 @@
+// console.log("");
+// for(let i=0; i<= 10 ; i++ ){
+//     console.log("malik");
+// }
+
+let op1= `malik`;
+let op2="siawish";
+
+let answer=`${op1} + ${op2}`;
+console.log(answer);
+console.log(answer.length);
+console.log(op2.toUpperCase());
