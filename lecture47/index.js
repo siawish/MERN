7ad42e09fe2ec6ec9d58a84962403f5ac5f0f25f
siawish <01-131232-025@student.bahria.edu.pk>
2025-03-01 13:01:52 +0500

    Classes && Default Parameters in Functions

---
+++ lecture47/index.js
@@ -0,0 +1,32 @@
+// class Human{
+    
+//     age =10;
+//     #wt= 10;
+//     ht= 100;
+
+//     walking(){
+//         console.log("I am walking.",this.#wt);
+//     }
+
+//     running(){
+//         console.log("i am running");
+//     }
+
+//     get getWeight(){
+//         return this.#wt;
+//     }
+
+//     set setWeight(val){
+//         this.#wt=val;
+//     }
+// }
+
+// let obj = new Human();
+
+// console.log(obj.getWeight);
+
+function sayName(myName="malik", lname="siawish"){
+    console.log("My name is:", myName, "and ", lname);
+}
+
+sayName();
