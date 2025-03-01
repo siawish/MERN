1d6908380f583df89cbd64371360fabdcd012454
siawish <01-131232-025@student.bahria.edu.pk>
2025-03-01 14:01:01 +0500

    Object Cloning && Garbage Collector in JS

---
+++ lecture49/index.js
@@ -0,0 +1,16 @@
+// console.log("babbar");
+
+let obj={
+    age:12,
+    wt:29,
+    ht:89
+}
+
+let dest= Object.assign({},obj);
+console.log(dest);
+// console.log(obj);
+// obj.color= "white";
+// console.log(obj);
+
+// let des={...obj};
+
